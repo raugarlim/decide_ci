951b799cbe01905cacb3243ea41702aaa217b0fe
raugarlim <raulgarcialimones@gmail.com>
2019-12-02 13:33:30 +0100

    pruebas

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -72,7 +72,7 @@ MODULES = [
 ]
 
 #BASEURL = 'http://localhost:8000'
-BASEURL = 'http://heroku-meet-travis.herokuapp.com'
+BASEURL = 'http://my-decide.herokuapp.com'
 
 APIS = {
     'authentication': BASEURL,
